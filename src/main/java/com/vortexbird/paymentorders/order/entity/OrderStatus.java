c3458c21a9e8bc76a3996b7fe75fe1381ce6bdd6
samuedo435 <samuedo435@gmail.com>
2026-06-10 10:22:58 -0500

    se añaden enumm para order

---
+++ src/main/java/com/vortexbird/paymentorders/order/entity/OrderStatus.java
@@ -0,0 +1,10 @@
+package com.vortexbird.paymentorders.order.entity;
+
+/**
+ * Estados permitidos para una orden de pago.
+ */
+public enum OrderStatus {
+    PENDING,
+    APPROVED,
+    REJECTED
+}
