0e6a83aeaaf89c42be13d81a30a0146ffc158e0c
samuedo435 <samuedo435@gmail.com>
2026-06-10 15:42:37 -0500

    se añade Service para cargar users desde db

---
+++ src/main/java/com/vortexbird/paymentorders/security/service/CustomUserDetailsService.java
@@ -0,0 +1,40 @@
+package com.vortexbird.paymentorders.security.service;
+
+import com.vortexbird.paymentorders.user.entity.User;
+import com.vortexbird.paymentorders.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Permite a Spring Security cargar usuarios
+ * desde la base de datos.
+ */
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found"));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                List.of(
+                        new SimpleGrantedAuthority(
+                                "ROLE_" + user.getRole().name()
+                        )
+                )
+        );
+    }
+}
